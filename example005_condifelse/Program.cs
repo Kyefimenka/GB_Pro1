d19ae05a2d3a5e23c0aa383a3b36176f112db9a8
Katsiaryna <caelpi85@gmail.com>
2022-06-22 01:34:59 +0200

    add example005_condifelse

---
+++ example005_condifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "bob")
+{
+    Console.WriteLine ("That is Bob");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console.WriteLine(username);
+}
