5a8dbf72cb9b1764d56e2594f0d019bc18c22ff3
Katsiaryna <caelpi85@gmail.com>
2022-06-24 15:40:27 +0200

    add example006

---
+++ example006_Max/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 3;
+int c = 8;
+int d = 1;
+int e = 5;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("max = ");
+Console.WriteLine(max);
