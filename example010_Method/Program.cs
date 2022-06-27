a6f818dbead1afa7b483ce95d37a0057111c9406
Katsiaryna <caelpi85@gmail.com>
2022-06-27 17:01:05 +0200

    add example010_Method

---
+++ example010_Method/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 12, 34, 45, 32, 21, 43, 45};
+int n = array.Length;
+int find = 45;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
